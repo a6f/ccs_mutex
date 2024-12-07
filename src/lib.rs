43f402aeaadb11f1e9d731cc61e8506094eca260
Ambrose Feinstein <ambrose@osdyne.com>
2024-12-07 15:13:09 -0800

    in terms of parking_lot

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 use core::fmt::{Debug, Display};
 use core::ops::{Deref, DerefMut};
 use core::time::Duration;
-pub use std::sync::WaitTimeoutResult;
 
-pub struct Mutex<T>(std::sync::Mutex<T>, std::sync::Condvar);
+pub struct Mutex<T>(parking_lot::Mutex<T>, parking_lot::Condvar);
 
-pub struct MutexGuard<'a, 'b, T>(Option<std::sync::MutexGuard<'b, T>>, &'a std::sync::Condvar);
+pub struct MutexGuard<'a, 'b, T>(parking_lot::MutexGuard<'b, T>, &'a parking_lot::Condvar);
 
 impl<T> Mutex<T> {
     pub fn new(t: T) -> Self {
@@ -13,45 +12,39 @@ impl<T> Mutex<T> {
     }
 
     pub fn lock(&self) -> MutexGuard<T> {
-        let guard = self.0.lock().unwrap();
-        MutexGuard(Some(guard), &self.1)
+        let guard = self.0.lock();
+        MutexGuard(guard, &self.1)
     }
 
     pub fn lock_when(&self, condition: impl Fn(&T) -> bool) -> MutexGuard<T> {
-        let guard = self.0.lock().unwrap();
-        let guard = self.1.wait_while(guard, |t| !condition(t)).unwrap();
-        MutexGuard(Some(guard), &self.1)
+        let mut guard = self.0.lock();
+        self.1.wait_while(&mut guard, |t| !condition(t));
+        MutexGuard(guard, &self.1)
     }
 
     pub fn lock_when_with_timeout(
         &self,
         condition: impl Fn(&T) -> bool,
         timeout: Duration,
-    ) -> Result<MutexGuard<T>, WaitTimeoutResult> {
-        let guard = self.0.lock().unwrap();
-        let (guard, timed_out) = self
+    ) -> Option<MutexGuard<T>> {
+        let mut guard = self.0.lock();
+        let timed_out = self
             .1
-            .wait_timeout_while(guard, timeout, |t| !condition(t))
-            .unwrap();
+            .wait_while_for(&mut guard, |t| !condition(t), timeout);
         match timed_out.timed_out() {
-            true => Err(timed_out),
-            false => Ok(MutexGuard(Some(guard), &self.1)),
+            true => None,
+            false => Some(MutexGuard(guard, &self.1)),
         }
     }
 
     pub fn try_lock(&self) -> Option<MutexGuard<T>> {
-        self.0
-            .try_lock()
-            .ok()
-            .map(|guard| MutexGuard(Some(guard), &self.1))
+        self.0.try_lock().map(|guard| MutexGuard(guard, &self.1))
     }
 }
 
 impl<T> MutexGuard<'_, '_, T> {
     pub fn await_condition(&mut self, condition: impl Fn(&T) -> bool) {
-        let guard = self.0.take().unwrap();
-        let guard = self.1.wait_while(guard, |t| !condition(t)).unwrap();
-        self.0 = Some(guard);
+        self.1.wait_while(&mut self.0, |t| !condition(t));
     }
 
     pub fn await_with_timeout(
@@ -59,13 +52,10 @@ impl<T> MutexGuard<'_, '_, T> {
         condition: impl Fn(&T) -> bool,
         timeout: Duration,
     ) -> bool {
-        let guard = self.0.take().unwrap();
-        let (guard, timed_out) = self
+        !self
             .1
-            .wait_timeout_while(guard, timeout, |t| !condition(t))
-            .unwrap();
-        self.0 = Some(guard);
-        !timed_out.timed_out()
+            .wait_while_for(&mut self.0, |t| !condition(t), timeout)
+            .timed_out()
     }
 }
 
@@ -95,13 +85,13 @@ impl<T: Debug> Debug for Mutex<T> {
 impl<T> Deref for MutexGuard<'_, '_, T> {
     type Target = T;
     fn deref(&self) -> &T {
-        self.0.as_ref().unwrap().deref()
+        self.0.deref()
     }
 }
 
 impl<T> DerefMut for MutexGuard<'_, '_, T> {
     fn deref_mut(&mut self) -> &mut T {
-        self.0.as_mut().unwrap().deref_mut()
+        self.0.deref_mut()
     }
 }
 
